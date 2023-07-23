f08ca2d37e85d6c5067e0353cb42e5c269dccf13
Valeria Vagapova <lera.vagapova@gmail.com>
2023-07-23 19:25:00 +0300

    add while loop to routine, change var name

--- philo/philosophers.h
+++ philo/philosophers.h
@@ -13,8 +13,8 @@ typedef struct s_philosopher
 	pthread_t 			thread;
 	pthread_mutex_t		*fork_own;
 	pthread_mutex_t		*fork_borrowed;
-	int					dead;
-	int					meal_count;
+	int					meal_count; // chenge to meals_left?
+	unsigned int		index; // TODO
 	unsigned long long	time_last_ate;
 	t_party				*party;
 }						t_philosopher;
@@ -25,11 +25,13 @@ typedef struct s_party
 	unsigned long long	time_to_die;
 	unsigned long long	time_to_eat;
 	unsigned long long	time_to_sleep;
-	int					number_of_meals;
+	int					number_of_meals_needed;
 	t_philosopher		*philosophers;
+	pthread_t 			monitoring_thread; // TODO
 	pthread_mutex_t		*forks;
 	pthread_mutex_t		guard;
 	pthread_mutex_t		printing;
+	pthread_mutex_t		dying;
 	int					someone_dead;
 }						t_party;
 
