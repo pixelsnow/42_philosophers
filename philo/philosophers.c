e33d2f9a0163e06156544074125d358eb327092d
lionel clerc <lclerc@student.hive.fi>
2023-07-25 10:26:50 +0300

    added join_{philo,monitoring}_threads functions

--- philo/philosophers.c
+++ philo/philosophers.c
@@ -1,5 +1,52 @@
 #include "philosophers.h"
 
+// Function to join philosopher threads
+
+/**
+ * @brief Joins all philosopher threads.
+ *
+ * This function is responsible for joining all the philosopher threads created
+ * by the party. It iterates through the `party->philosophers` array and calls
+ * `pthread_join` on each philosopher's thread. If an error occurs during
+ * joining, it prints an error message indicating which philosopher thread
+ * failed to join.
+ *
+ * @param party Pointer to the `t_party` struct holding party information
+ */
+void	join_philosopher_threads(t_party *party)
+{
+	unsigned int	i;
+
+	for (i = 0; i < party->number_of_philosophers; i++)
+	{
+		printf("Joining philosopher thread %u\n", i);
+		if (pthread_join(party->philosophers[i].thread, NULL) != SUCCESS)
+		{
+			printf("Failed to join philosopher thread %u\n", i);
+		}
+		printf("Joined philosopher thread %u -> SUCCESS\n", i);
+	}
+}
+
+/**
+ * @brief Joins the monitoring thread.
+ *
+ * This function is responsible for joining the monitoring thread created by
+ * the party. It calls `pthread_join` on the `party->monitoring_thread`. If an
+ * error occurs during joining, it prints an error message.
+ *
+ * @param party Pointer to the `t_party` struct holding party information
+ */
+void	join_monitoring_thread(t_party *party)
+{
+	printf("Joining monitoring thread\n");
+	if (pthread_join(party->monitoring_thread, NULL) != SUCCESS)
+	{
+		printf("Failed to join monitoring thread\n");
+	}
+	printf("Joined monitoring thread -> SUCCESS\n");
+}
+
 // Main thread that runs the party and starts all the threads
 /**
  * @brief	Function to run the philosopher party and start all the threads
@@ -30,9 +77,9 @@
  * @param party	Pointer to the `t_party` struct holding party information
  * @return int	`SUCCESS` if the party runs successfully, otherwise `ERROR`
  */
-int run_party(t_party *party)
+int	run_party(t_party *party)
 {
-	unsigned int		i;
+	unsigned int	i;
 
 	// Start all threads simultaneously
 	pthread_mutex_lock(&(party->guard));
@@ -44,46 +91,50 @@ int run_party(t_party *party)
 		{
 			//How do we set the end of party here?
 			pthread_mutex_unlock(&(party->guard));
-			break;
+			break ;
 		}
 		i++;
 	}
 	start_monitoring(party);
-	
 	printf("unlocking guard...\n");
 	pthread_mutex_unlock(&(party->guard));
-
 	/* Issue now is that main thread doesn't wait for monitoring and philosophers to stop.
 	if we remove usleep(1000) Main snatches party->party_going_on before monitoring gets to it
 	and therefore gets to cleanup while party is still ongoing. */
+	//printf("Main thread waiting...\n");
+	//usleep(1000);
+	//pthread_mutex_lock(&(party->party_going_on));
+	//pthread_mutex_unlock(&(party->party_going_on));
 	printf("Main thread waiting...\n");
-	usleep(1000);
-	pthread_mutex_lock(&(party->party_going_on));
-	pthread_mutex_unlock(&(party->party_going_on));
+	join_philosopher_threads(party);
+	join_monitoring_thread(party);
 	printf("Main thread continuing...\n");
 	/* I'm wondering if the solution is that monitoring should trigger cleanup and everything instead,
 	and the main thread should just be allowed to die in peace? Or is that illegal */
-	
 	return (SUCCESS);
 }
 
 /**
- * @brief	Entry point of the program
- * @details	
+ * @brief Entry point of the program.
  *
- * @param ac 
- * @param av 
- * @return 
+ * This is the main function of the program. It initializes the `t_party`
+ * struct, parses the command-line arguments using `parse_args`, prepares the
+ * party using `prepare_party`, runs the party using `run_party`, and finally
+ * cleans up the resources using `clean_up`.
+ *
+ * @param ac Number of command-line arguments
+ * @param av Array of command-line argument strings
+ * @return int `SUCCESS` if the program runs successfully, otherwise `ERROR`
  */
 int	main(int ac, char **av)
 {
 	t_party	party;
-	
+
 	if (parse_args(&party, ac, av))
 	{
 		return (ERROR);
 	}
-	// Init 
+	// Init
 	if (prepare_party(&party))
 	{
 		// prepare_party currenty actually always returns success (i think)
@@ -95,5 +146,5 @@ int	main(int ac, char **av)
 		return (ERROR);
 	}
 	clean_up(&party);
-	return(SUCCESS);
+	return (SUCCESS);
 }
